1f0eae360693b11e643d8f4ff64545130a434c07
Marcin Sulecki <marcin.sulecki@gmail.com>
2020-06-15 12:57:33 +0200

    Builder - SalesReport

--- src/DesignPatterns/BuilderPattern/Program.cs
+++ src/DesignPatterns/BuilderPattern/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Xml;
 
 namespace BuilderPattern
@@ -16,7 +17,9 @@ namespace BuilderPattern
 
             //PhoneTest();
 
-            SalesReportTest();
+            // SalesReportTest();
+
+            SalesReportBuilderTest();
         }
 
         private static void SalesReportTest()
@@ -41,6 +44,27 @@ namespace BuilderPattern
                 .SelectMany(o => o.Details)
                 .GroupBy(o => o.Product)
                 .Select(g => new ProductReportDetail(g.Key, g.Sum(p => p.Quantity), g.Sum(p => p.LineTotal)));
+
+            Console.WriteLine(salesReport);
+
+        }
+
+        private static void SalesReportBuilderTest()
+        {
+            FakeOrdersService ordersService = new FakeOrdersService();
+            IEnumerable<Order> orders = ordersService.Get();
+
+            ISalesReportBuilder salesReportBuilder = new SalesReportBuilder(orders);
+
+            salesReportBuilder.AddHeader();
+            salesReportBuilder.AddGenderTotals();
+
+            if (true)
+            {
+                salesReportBuilder.AddProductTotals();
+            }
+
+            SalesReportBase salesReport = salesReportBuilder.Build();
 
             Console.WriteLine(salesReport);
 
@@ -143,11 +167,9 @@ namespace BuilderPattern
 
     #endregion
 
-    public class SalesReport
+    public class SalesReport : SalesReportBase
     {
-        public string Title { get; set; }
-        public DateTime CreateDate { get; set; }
-        public decimal TotalSalesAmount { get; set; }
+        
 
         public IEnumerable<ProductReportDetail> ProductDetails { get; set; }
         public IEnumerable<GenderReportDetail> GenderDetails { get; set; }
@@ -155,27 +177,26 @@ namespace BuilderPattern
 
         public override string ToString()
         {
-            string output = string.Empty;
+            StringBuilder builder = new StringBuilder();
 
-            output += "------------------------------\n";
+            builder.AppendLine("------------------------------");
+            builder.AppendLine($"{Title} {CreateDate}");
+            builder.AppendLine($"Total Sales Amount: {TotalSalesAmount:c2}");
+            builder.AppendLine("------------------------------");
+            builder.AppendLine("Total By Products:");
 
-            output += $"{Title} {CreateDate}\n";
-            output += $"Total Sales Amount: {TotalSalesAmount:c2}\n";
-
-            output += "------------------------------\n";
-
-            output += "Total By Products:\n";
             foreach (var detail in ProductDetails)
             {
-                output += $"- {detail.Product.Name} {detail.Quantity} {detail.TotalAmount:c2}\n";
+                builder.AppendLine($"- {detail.Product.Name} {detail.Quantity} {detail.TotalAmount:c2}");
             }
-            output += "Total By Gender:\n";
+            builder.AppendLine("Total By Gender:");
             foreach (var detail in GenderDetails)
             {
-                output += $"- {detail.Gender} {detail.Quantity} {detail.TotalAmount:c2}\n";
+                builder.AppendLine($"- {detail.Gender} {detail.Quantity} {detail.TotalAmount:c2}");
             }
 
-            return output;
+            // Build
+            return builder.ToString();
         }
     }
 
---
+++ src/DesignPatterns/BuilderPattern/Solution.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuilderPattern
+{
+    // Abstract Builder
+    public interface ISalesReportBuilder
+    {
+        SalesReportBase Build();
+        void AddHeader();
+        void AddGenderTotals();
+        void AddProductTotals();
+    }
+
+    // Concrete Builder
+    public class SalesReportBuilder : ISalesReportBuilder
+    {
+        private IEnumerable<Order> orders;
+
+        private bool hasHeader;
+        private bool hasGenderTotals;
+        private bool hasProductTotals;
+
+        public SalesReportBuilder(IEnumerable<Order> orders)
+        {
+            this.orders = orders;            
+        }
+
+        public void AddHeader()
+        {
+            hasHeader = true;
+        }
+
+        public void AddGenderTotals()
+        {
+            hasGenderTotals = true;
+        }
+
+        public void AddProductTotals()
+        {
+            hasProductTotals = true;
+        }
+
+
+        // Create product
+        public SalesReportBase Build()
+        {
+            SalesReport salesReport = new SalesReport();
+
+            if (hasHeader)
+            {
+                salesReport.Title = "Raport sprzedaży";
+                salesReport.CreateDate = DateTime.Now;
+                salesReport.TotalSalesAmount = orders.Sum(s => s.Amount);
+            }
+
+            if (hasGenderTotals)
+            {
+                salesReport.GenderDetails = orders
+              .GroupBy(o => o.Customer.Gender)
+              .Select(g => new GenderReportDetail(
+                          g.Key,
+                          g.Sum(x => x.Details.Sum(d => d.Quantity)),
+                          g.Sum(x => x.Details.Sum(d => d.LineTotal))));
+            }
+
+            if (hasProductTotals)
+            {
+                salesReport.ProductDetails = orders
+              .SelectMany(o => o.Details)
+              .GroupBy(o => o.Product)
+              .Select(g => new ProductReportDetail(g.Key, g.Sum(p => p.Quantity), g.Sum(p => p.LineTotal)));
+            }
+
+            return salesReport;
+        }
+    }
+
+
+    // Abstract Product
+    public abstract class SalesReportBase
+    {
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+        public decimal TotalSalesAmount { get; set; }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+
+            output += "------------------------------\n";
+
+            output += $"{Title} {CreateDate}\n";
+
+            return output;
+        }
+    }
+
+}
