9985ab6d26121d156a29a5c9f67917c74ed31928
Marcin Sulecki <marcin.sulecki@gmail.com>
2020-06-15 14:34:09 +0200

    Refaktoring

---
+++ src/DesignPatterns/BuilderPattern/FluentPhone.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BuilderPattern
+{
+    // https://github.com/VerbalExpressions/CSharpVerbalExpressions
+
+    // https://github.com/FluentDateTime/FluentDateTime
+
+    public class FluentPhone : IFrom, ITo, ISubject, ICall
+    {
+        private string from;
+        private ICollection<string> tos;
+        private string subject;
+
+        protected FluentPhone()
+        {
+            tos = new Collection<string>();
+        }
+
+        public static IFrom On
+        {
+            get
+            {
+                return new FluentPhone();
+            }
+        }
+
+        public ITo From(string number)
+        {
+            this.from = number;
+
+            return this;
+        }
+
+        public ISubject To(string number)
+        {
+            this.tos.Add(number);
+
+            return this;
+        }
+
+        public ICall WithSubject(string subject)
+        {
+            this.subject = subject;
+
+            return this;
+        }
+
+
+        // Build
+        public void Call()
+        {
+            foreach (var to in tos)
+            {
+                if (!string.IsNullOrEmpty(subject))
+                    Call(from, to, subject);
+                else
+                    Call(from, to);
+            }
+        }
+
+        private void Call(string from, string to, string subject)
+        {
+            Console.WriteLine($"Calling from {from} to {to} with subject {subject}");
+        }
+
+        private void Call(string from, string to)
+        {
+            Console.WriteLine($"Calling from {from} to {to}");
+        }
+    }
+
+}
--- src/DesignPatterns/BuilderPattern/Solution.cs
+++ src/DesignPatterns/BuilderPattern/Solution.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -142,74 +141,6 @@ namespace BuilderPattern
     public interface ICall
     {
         void Call();
-    }
-
-    // https://github.com/VerbalExpressions/CSharpVerbalExpressions
-
-    // https://github.com/FluentDateTime/FluentDateTime
-
-    public class FluentPhone : IFrom, ITo, ISubject, ICall
-    {
-        private string from;
-        private ICollection<string> tos;
-        private string subject;
-
-        protected FluentPhone()
-        {
-            tos = new Collection<string>();
-        }
-
-        public static IFrom On
-        {
-            get
-            {
-                return new FluentPhone();
-            }
-        }
-
-        public ITo From(string number)
-        {
-            this.from = number;
-
-            return this;
-        }
-
-        public ISubject To(string number)
-        {
-            this.tos.Add(number);
-
-            return this;
-        }
-
-        public ICall WithSubject(string subject)
-        {
-            this.subject = subject;
-
-            return this;
-        }
-
-
-        // Build
-        public void Call()
-        {
-            foreach (var to in tos)
-            {
-                if (!string.IsNullOrEmpty(subject))
-                    Call(from, to, subject);
-                else
-                    Call(from, to);
-            }
-        }
-
-        private void Call(string from, string to, string subject)
-        {
-            Console.WriteLine($"Calling from {from} to {to} with subject {subject}");
-        }
-
-        private void Call(string from, string to)
-        {
-            Console.WriteLine($"Calling from {from} to {to}");
-        }
     }
 
 }
