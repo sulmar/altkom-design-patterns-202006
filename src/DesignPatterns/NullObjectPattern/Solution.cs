e246c4e45b99fa6ec92b447bf6c693792ac1500a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-18 12:22:29 +0200

    NullObjectPattern

---
+++ src/DesignPatterns/NullObjectPattern/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullObjectPattern.Solution
+{
+
+    public class NullObjectPatternTest
+    {
+        public static void Test()
+        {
+            IProductRepository productRepository = new FakeProductRepository();
+
+            ProductBase product = productRepository.Get(1);
+
+            product.RateIt(3);
+        }
+    }
+
+
+    // Abstract Object
+    public abstract class ProductBase
+    {
+        public abstract void RateIt(int rate);
+    }
+
+    // Real Object
+    public class Product : ProductBase
+    {
+        private int rate;
+
+        public override void RateIt(int rate)
+        {
+            // aktualizacja oceny
+            this.rate = rate;
+        }
+    }
+
+    public class NullProduct : ProductBase
+    {
+        public override void RateIt(int rate)
+        {
+            // nic nie rób
+        }
+    }
+
+    public interface IProductRepository
+    {
+        ProductBase Get(int id);
+    }
+
+    public class FakeProductRepository : IProductRepository
+    {
+        public ProductBase Get(int id)
+        {
+            return new NullProduct();
+        }
+    }
+}
